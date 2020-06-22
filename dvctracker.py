9e738922b9da564b16dbab5327f641a641534bf2
George Waters <gwatersdev@gmail.com>
2020-06-22 17:41:38 -0400

    Revert "Add update_source function temporarily for this update"
    This reverts commit 8ee8b10e82539c8736efb807368b124662714c49.
    
    Removes update_source function.

--- dvctracker.py
+++ dvctracker.py
@@ -1,7 +1,6 @@
 from flask_migrate import Migrate, upgrade
-from flask.cli import with_appcontext
 from app import create_app, db
-from app.models import StoredSpecial, Status, Email, PhoneNumber, SpecialTypes, ParserStatus
+from app.models import StoredSpecial, Status, Email, PhoneNumber, SpecialTypes
 from app.parsers import DVCRentalPointParser, ParsedSpecial
 from app.cli import update_specials
 import os
@@ -24,16 +23,7 @@ def deploy():
     print('Upgrading db schema if any changes made...')
     upgrade()
 
-    # This is just temporary for this version
-    update_source()
-
     #run an update to the specials, if the db changed we may now track more
     #data and need to update to get it
     print('Upgrading stored specials with live data...')
     update_specials((), False, False)
-
-def update_source():
-    for special in StoredSpecial.query:
-        special.source_name = special.source
-        special.source = 'dvcrentalstore_preconfirms' if special.type == 'preconfirm' else 'dvcrentalstore_points'
-    db.session.commit()
