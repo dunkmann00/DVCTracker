fa5310171e662b6f569c34683e41303793fb13bb
George Waters <gwatersdev@gmail.com>
2022-06-10 00:03:08 -0400

    Adds APNS related keys
    Also adds Flask's SECRET_KEY...this should have already been there

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://', 1)
